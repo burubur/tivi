1086eb837f205cc6625975ac5b6c88e8d16d1050
Burhan Mubarok <burhanmubarok@gmail.com>
2019-03-05 01:51:43 +0700

    tv discover page init

---
+++ components/discover/detail.js
@@ -0,0 +1,153 @@
+import React, { Component } from "react"
+import { Row, Col, Card, List, Icon } from "antd"
+import { twoColumn } from "./../constants/flex"
+
+const APIKEY = "73be4d439ae0fc2041dab7522c37c14f"
+const BASE_URL = "http://127.0.0.1:6006"
+
+class DiscoveryDetail extends Component {
+    state = {
+        loading: false,
+        item: {
+
+        },
+        detail: {
+            episodes: [],
+        },
+    }
+    componentDidMount () {
+        let tvID = this.props.match.params.id
+        this.setState({
+            loading: true,
+        })
+        fetch(`${BASE_URL}/tv/${tvID}?api_key=${APIKEY}`)
+        .then(response => (
+            response.json()
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    item: data,
+                })
+            })
+            .catch(e => {
+                console.log("failed to parse the response, got: ", e)
+            })
+        ))
+        .catch(e => {
+            console.log("failed on API request, got: ", e)
+        })
+        
+        fetch(`${BASE_URL}/tv/${tvID}/season/1?api_key=${APIKEY}`)
+        .then(response => (
+            response.json()
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    detail: data,
+                })
+            })
+            .catch(e => {
+                console.log("failed to parse the response, got: ", e)
+            })
+        ))
+        .catch(e => {
+            console.log("failed on API request, got: ", e)
+        })
+    }
+    
+    render() {
+        let summary = {...this.state.item, ...this.state.detail}
+        const action = [
+            <Icon type="share-alt" key={summary.id} />,
+            <Icon type="eye" key={summary.id} />,
+            <Icon type="star" key={summary.id} />
+        ]
+
+        return (
+            <div>
+                <Row type="flex" justify="center" gutter={24}>
+                    <Col {...twoColumn}>
+                        <Card
+                            loading={this.state.loading}
+                            bordered={false}
+                            style={{borderRadius: 8}}
+                            cover={<img alt="cover" src={("https://image.tmdb.org/t/p/w500/"+this.state.item.poster_path)} style={{borderTopLeftRadius: 8, borderTopRightRadius: 8}} />}
+                            actions={action}
+                        >
+                            <Card.Meta
+                                title={this.state.item.name}
+                                description={<Desc {...this.state.item} />}
+                            />
+                        </Card>
+                    </Col>
+                    <Col {...twoColumn}>
+                        <Episodes {...summary} />
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
+
+const Desc = (data) => {
+    let dt = new Date(data.first_air_date)
+    return (
+        <div>
+            <span>Year: {dt.getFullYear()}</span>
+            <br />
+            <span>Seasson: {data.number_of_seasons}</span>
+            <br />
+            <span>Episode: {data.number_of_episodes}</span>
+        </div>
+    )
+}
+
+class Episodes extends Component {
+    render() {
+        let data = this.props
+        return (
+            <List
+                loading={data.loading}
+                itemLayout="vertical"
+                size="small"
+                bordered
+                dataSource={data.episodes}
+                renderItem={itemData => <EpisodeItem {...itemData} />}
+                header={<EpisodeHeader {...data} />}
+            >
+            </List>
+        )
+    }
+}
+
+const EpisodeHeader = (data) => (
+    <div>
+        <span style={{fontWeight: "bold", fontSize: 20}}>Episodes</span>
+        <br />
+        <span>Seasson: {<SeasonLink {...data} />}</span>
+    </div>
+)
+
+const EpisodeItem = (data) => {
+    return (
+        <List.Item
+            extra={<Icon style={{fontSize: 24}} type="play-circle" onClick={(data) => console.log("let's watch...")}/>}
+            >
+            {data.episode_number}. {data.name}
+        </List.Item>
+    )
+}
+
+export {
+    DiscoveryDetail
+}
+
+const SeasonLink = seasons => {
+    let seasonLink = []
+    for(let i=1, l=seasons.number_of_seasons; i <= l; i++){
+        seasonLink.push(i);
+    }
+    return (
+        <span>{seasonLink.map(link => <span key={link} style={{fontWeight: "bold"}}>{link} </span>)}</span>
+    )
+}
---
+++ components/discover/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import InfiniteScroll from "react-infinite-scroller"
+import { Col, Row, Card, Icon, Spin } from "antd"
+import { Multiple } from "./../constants"
+
+@connect(store => store)
+class Discover extends Component {
+    componentDidMount() {
+        if (this.props.tvDiscover.page === 0) {
+            this.load(1)
+        }
+    }
+
+    showDetail(id) {
+        this.props.history.push("/discovery/"+id)
+    }
+
+    load(page) {
+        this.props.dispatch({
+            type: "tvDiscover/load",
+            payload: page || 1
+        })
+    }
+
+    render() {
+        let { tvDiscover } = this.props
+        return (
+            <div>
+                <InfiniteScroll
+                    initialLoad={false}
+                    pageStart={1}
+                    loadMore={page => this.load(page)}
+                    hasMore={!tvDiscover.loading && tvDiscover.hasMore}
+                    useWindow={true}
+                >
+                    <Row type="flex" justify="center" gutter={16}>
+                        {Array.from(tvDiscover.results).map(item => (
+                            <Col {...Multiple} key={item.id} onClick={() => this.showDetail(item.id)}>
+                                <TvDiscover {...item} />
+                            </Col>
+                        ))}
+                    </Row>
+                    <Spin spinning={tvDiscover.loading} style={{minWidth: "100%"}} />
+                </InfiniteScroll>
+                <div style={{textAlign: "center", minWidth: "100vw"}}>
+                    {!tvDiscover.hasMore ? (<span>Wohoo... you completely loaded all TV Shows</span>) : null}
+                </div>
+            </div>
+        )
+    }
+}
+
+class TvDiscover extends Component {
+    render() {
+        let data = this.props
+        return (
+            <div style={{ marginBottom: "16px" }}>
+                <Card
+                    bordered={false}
+                    cover={<Image path={data.backdrop_path} />}
+                    key={data.id}
+                    style={{borderRadius: 8}}
+                    hoverable
+                >
+                    <Card.Meta
+                        key={data.id}
+                        title={data.name}
+                        description={<span><Icon type="star" theme="twoTone" twoToneColor="#eb2f96"/> {data.vote_average}</span>}
+                    />
+                </Card>
+            </div>
+        )
+    }
+}
+
+
+const Image = (prop) => {
+    const path = `https://image.tmdb.org/t/p/w500${prop.path}`
+    return (
+        <img alt="cover" src={path} style={{borderTopLeftRadius: 8, borderTopRightRadius: 8}} />
+    )
+}
+
+export default Discover
