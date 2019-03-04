6f60e5fa3f87d6ce61e4585d90595a0703df8c3e
Burhan Mubarok <burhanmubarok@gmail.com>
2019-03-05 01:50:08 +0700

    create a middleware that listen to some of reducer

---
+++ stores/middleware.js
@@ -0,0 +1,45 @@
+import { all, put, takeEvery } from "redux-saga/effects"
+import { searchTvDiscover, loadTvDiscover } from "./api" 
+
+function * search ({payload}) {
+    try {
+        const response = yield searchTvDiscover(payload)
+        yield put({
+            type: "tvDiscover/searched",
+            payload: response
+        })
+    } catch (e) {
+        console.log("error on searching service, got:", e)
+    }
+}
+
+function * fetchTvDiscover ({payload}) {
+    try {
+        const response = yield loadTvDiscover(payload)
+        yield put({
+            type: "tvDiscover/loaded",
+            payload: response
+        })
+    } catch (e) {
+        console.log("error on searching service, got:", e);
+    }
+}
+
+function * tvDiscover () {
+    yield takeEvery("tvDiscover/search", search)
+    yield takeEvery("tvDiscover/load", fetchTvDiscover)
+}
+
+
+function * wellcome () {
+    yield console.log("redux-saga middleware applied!")
+}
+
+const middleware = function * sagas () {
+    yield all([
+        wellcome(),
+        tvDiscover(),
+    ])
+}
+
+export default middleware
