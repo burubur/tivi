2e9b1b4306c7f7378e3b783c84879a2d9735d247
Burhan Mubarok <burhanmubarok@gmail.com>
2019-03-05 01:50:47 +0700

    provide some constants

---
+++ components/constants/flex.js
@@ -0,0 +1,32 @@
+const Single = {
+    xs: { span: 22 },
+    sm: { span: 20 },
+    md: { span: 18 },
+    lg: { span: 18 },
+    xl: { span: 12 },
+    xxl: { span: 8 }
+}
+
+const Multiple = {
+    xs: { span: 24 },
+    sm: { span: 20 },
+    md: { span: 5 },
+    lg: { span: 5 },
+    xl: { span: 5 },
+    xxl: { span: 5 }
+}
+
+const twoColumn = {
+    xs: { span: 22 },
+    sm: { span: 20 },
+    md: { span: 18 },
+    lg: { span: 18 },
+    xl: { span: 12 },
+    xxl: { span: 8 }
+}
+
+export {
+    Single,
+    Multiple,
+    twoColumn,
+}
