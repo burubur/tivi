7540e2d42e60851c72450d4290b3c3072b7063cb
Burhan Mubarok <burhanmubarok@gmail.com>
2019-03-07 18:09:59 +0700

    cange single flex size

--- components/constants/flex.js
+++ components/constants/flex.js
@@ -1,8 +1,8 @@
 const Single = {
-    xs: { span: 22 },
-    sm: { span: 20 },
-    md: { span: 18 },
-    lg: { span: 18 },
+    xs: { span: 16 },
+    sm: { span: 16 },
+    md: { span: 16 },
+    lg: { span: 12 },
     xl: { span: 12 },
     xxl: { span: 8 }
 }
